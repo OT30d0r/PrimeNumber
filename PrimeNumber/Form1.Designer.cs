bc7b98bfc4f80f020538c5fd34aa1c4ba3d31374
unknown <teodor.oprea@cncn.ro>
2024-04-03 19:33:41 +0300

    PrimeNumber

---
+++ PrimeNumber/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PrimeNumber
+{
+    partial class PrimeNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.PrimeNumberTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(278, 268);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(219, 109);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.Text = "Check";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // PrimeNumberTextBox
+            // 
+            this.PrimeNumberTextBox.Location = new System.Drawing.Point(317, 165);
+            this.PrimeNumberTextBox.Name = "PrimeNumberTextBox";
+            this.PrimeNumberTextBox.Size = new System.Drawing.Size(143, 22);
+            this.PrimeNumberTextBox.TabIndex = 1;
+            // 
+            // PrimeNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PrimeNumberTextBox);
+            this.Controls.Add(this.CheckButton);
+            this.Name = "PrimeNumberForm";
+            this.Text = "PrimeNumber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.TextBox PrimeNumberTextBox;
+    }
+}
+
---
+++ PrimeNumber/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PrimeNumber
+{
+    public partial class PrimeNumberForm : Form
+    {
+        public PrimeNumberForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrimeNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Resetăm culoarea de fundal la implicit
+            this.BackColor = SystemColors.Control;
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            bool isNumber = Int32.TryParse(PrimeNumberTextBox.Text, out result);
+            if (isNumber)
+            {
+                if (result > 1)
+                {
+                    bool isPrime = true;
+                    for (int i = 2; i <= result / 2; i++)
+                    {
+                        if (result % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        TrueOrFalseLabel.Text = "true";
+                        this.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        TrueOrFalseLabel.Text = "false";
+                        this.BackColor = Color.Red;
+                    }
+                }
+
+            }
+            else
+            {
+                TrueOrFalseLabel.Text = "false";
+                this.BackColor = Color.Yellow;
+                MessageBox.Show("Nu s-a introdus un număr întreg.");
+            }
+        }
+    }
+}
